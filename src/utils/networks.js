8c44d7cde67aa5d22cc711d2c0b731655419b57e
yathishram <123yathish.r@gmail.com>
2022-04-12 15:05:01 +0530

    updates network URLs

--- src/utils/networks.js
+++ src/utils/networks.js
@@ -1,3 +1,7 @@
+import dotenv from 'dotenv'
+dotenv.config()
+
+
 export const networks = {
   "localhost": {
     "chainId": 31337,
@@ -9,7 +13,7 @@ export const networks = {
   },
   "mainnet": {
     "chainId": 1,
-    "url": "http://localhost:8545",
+    "url": `https://mainnet.infura.io/v3/${process.env.INFURA_API_KEY}`,
     "addresses": {
       "AutoAppealableArbitrator": "",
       "SafientMain": ""
@@ -17,7 +21,7 @@ export const networks = {
   },
   "rinkeby": {
     "chainId": 4,
-    "url": "http://localhost:8545",
+    "url": "",
     "addresses": {
       "AutoAppealableArbitrator": "0xf54D6b97749ECD28F9EbF836Ed9cE0C387a2f0A1",
       "SafientMain": "0x2fF492C5D9d071a2bed17e30418aF128f22e42fA"
@@ -25,7 +29,7 @@ export const networks = {
   },
   "kovan": {
     "chainId": 42,
-    "url": "http://localhost:8545",
+    "url": `https://kovan.infura.io/v3/${process.env.INFURA_API_KEY}`,
     "addresses": {
       "AutoAppealableArbitrator": "0xf54D6b97749ECD28F9EbF836Ed9cE0C387a2f0A1",
       "SafientMain": "0xF88915c8762aB973b64086221cDFd20Ec104F732"
@@ -33,7 +37,7 @@ export const networks = {
   },
   "ropsten": {
     "chainId": 3,
-    "url": "http://localhost:8545",
+    "url": "",
     "addresses": {
       "AutoAppealableArbitrator": "0xf54D6b97749ECD28F9EbF836Ed9cE0C387a2f0A1",
       "SafientMain": "0xC0B0591Eac7887F443cef0f28BB818E454eD6637"
@@ -41,7 +45,7 @@ export const networks = {
   },
   "polygontestnet": {
     "chainId": 80001,
-    "url": "http://localhost:8545",
+    "url": `https://matic-mumbai.chainstacklabs.com`,
     "addresses": {
       "AutoAppealableArbitrator": "0xf54D6b97749ECD28F9EbF836Ed9cE0C387a2f0A1",
       "SafientMain": "0xccA53824ce8944a3cB7b21139bd177DEEe6e27C0"
